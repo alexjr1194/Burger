63ad06eb7fe617ff2339c7ca00e655e5bfcdee9d
alexjr1194 <alexjr1194@me.com>
2017-10-24 12:12:48 -0700

    fixed code for schema.sql && seeds.sql

---
+++ db/seeds.sql
@@ -0,0 +1,3 @@
+INSERT INTO burgers(burger_name,devoured,date_created) VALUES('Cheese Burger', 0, '2017-10-23');
+INSERT INTO burgers(burger_name,devoured,date_created) VALUES('Double Cheese Burger', 0, '2017-10-22');
+INSERT INTO burgers(burger_name,devoured,date_created) VALUES('Bacon Cheese Burger', 1, '2017-10-22');
