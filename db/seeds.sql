e1ccaed9101c03335c006c0c72fdb32f367c0d1c
alexjr1194 <alexjr1194@me.com>
2017-10-25 01:20:23 -0700

    fix to seeds.sql

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,3 +1,4 @@
+use burgers_db;
 INSERT INTO burgers(burger_name,devoured,date_created) VALUES('Cheese Burger', 0, '2017-10-23');
 INSERT INTO burgers(burger_name,devoured,date_created) VALUES('Double Cheese Burger', 0, '2017-10-22');
 INSERT INTO burgers(burger_name,devoured,date_created) VALUES('Bacon Cheese Burger', 1, '2017-10-22');
