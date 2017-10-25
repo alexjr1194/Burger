c5dc46b8313df935a4c067ea09253788063304a4
alexjr1194 <alexjr1194@me.com>
2017-10-25 12:38:04 -0700

    made small fix to code

--- models/burger.js
+++ models/burger.js
@@ -1,8 +1,8 @@
 var orm = require('../config/orm.js');
 
 var burgers = {
-  showAll: function (cb) {
-    orm.showAll('burgers', function (res) {
+  selectAll: function (cb) {
+    orm.selectAll('burgers', function (res) {
       cb(res);
     });
   },
